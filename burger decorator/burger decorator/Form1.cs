97260e5c534bf21560fa4c9b9202255185e5efe4
MAXARAJAA <114005737+MAXARAJAA@users.noreply.github.com>
2023-03-12 14:04:57 +0200

    Add files via upload

---
+++ burger decorator/burger decorator/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using burger_decorator;
+
+namespace burger_decorator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Burger burger1 = new ItalianBurger();
+            burger1 = new TomatoBurger(burger1); // итальянская пицца с томатами
+            MessageBox.Show("Название: " + burger1.Name);
+            MessageBox.Show("Цена: "+ burger1.GetCost());
+
+            Burger burger2 = new ItalianBurger();
+            burger2 = new CheeseBurger(burger2);// итальянская пиццы с сыром
+            MessageBox.Show("Название: "+ burger2.Name);
+            MessageBox.Show("Цена: "+ burger2.GetCost());
+
+            Burger burger3 = new BulgerianBurger();
+            burger3 = new TomatoBurger(burger3);
+            burger3 = new CheeseBurger(burger3);// болгарская пиццы с томатами и сыром
+            MessageBox.Show("Название: "+ burger3.Name);
+            MessageBox.Show("Цена: "+ burger3.GetCost());
+
+            
+            
+        }
+    }
+}
---
+++ burger decorator/burger decorator/burger.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace burger_decorator
+{
+    
+        abstract class Burger
+    {
+            public Burger(string n)
+            {
+                this.Name = n;
+            }
+            public string Name { get; protected set; }
+            public abstract string GetCost();
+        }
+
+        class ItalianBurger : Burger
+    {
+            public ItalianBurger() : base("Итальянский бургер")
+            { }
+            public override string GetCost()
+            {
+                return "10";
+            }
+        }
+        class BulgerianBurger : Burger
+    {
+            public BulgerianBurger()
+                : base("Болгарсккий бургер")
+            { }
+            public override string GetCost()
+            {
+                return "8";
+            }
+        }
+
+        abstract class BurgerDecorator : Burger
+    {
+            protected Burger burger;
+            public BurgerDecorator(string n, Burger burger) : base(n)
+            {
+                this.burger = burger;
+            }
+        }
+
+        class TomatoBurger : BurgerDecorator
+    {
+            public TomatoBurger(Burger p)
+                : base(p.Name + ", с томатами", p)
+            { }
+
+            public override string GetCost()
+            {
+                return burger.GetCost() + "3";
+            }
+        }
+
+        class CheeseBurger : BurgerDecorator
+    {
+            public CheeseBurger(Burger p)
+                : base(p.Name + ", с сыром", p)
+            { }
+
+            public override string GetCost()
+            {
+                return burger.GetCost() + "5";
+            }
+        }
+    
+}
